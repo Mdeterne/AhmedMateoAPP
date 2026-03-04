95935ca625359991aefd35d5ae8b8dd767a5b412
Mateo Deterne <mateodeterne@gmail.com>
2026-03-04 17:19:58 +0100

    Initial commit

---
+++ AhmedMateoAPP/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace AhmedMateoAPP;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
